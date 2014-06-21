6cd715fad703497fd21e8f8376c0681b3834beee
Jonathan Da Silva <dasilvabastos.jonathan@gmail.com>
2014-06-21 19:58:23 +0200

    Use Convert on the AnimeParser.

--- app/services/Parser/Anime.php
+++ app/services/Parser/Anime.php
@@ -2,6 +2,7 @@
 namespace Services\Parser;
 use \Dom;
 use \Http;
+use \Convert;
 
 class Anime
 {
@@ -14,18 +15,18 @@ class Anime
         Dom::get('anime')->each(function($anime) use (&$results){
             $result = [
                 'serie' => [
-                    'id'       => (int)    $anime->series_animedb_id->text,
-                    'title'    => (string) $anime->series_title->text,
-                    'episodes' => (int)    $anime->series_episodes->text,
-                    'synonyms' => (array)  explode(';', $anime->series_synonyms->text),
-                    'type'     => (int)    $anime->series_type->text,
-                    'status'   => (int)    $anime->series_status->text,
-                    'image'    => (string) $anime->series_image->text,
+                    'id'       => Convert::toInt   ($anime->series_animedb_id->text),
+                    'title'    => Convert::toString($anime->series_title->text),
+                    'episodes' => Convert::toString($anime->series_episodes->text),
+                    'synonyms' => Convert::toArray ($anime->series_synonyms->text, ';'),
+                    'type'     => Convert::toString($anime->series_type->text),
+                    'status'   => Convert::toInt   ($anime->series_status->text),
+                    'image'    => Convert::toString($anime->series_image->text),
                 ],
                 'user' => [
-                    'episodes' => (int)    $anime->my_watched_episodes->text,
-                    'score'    => (int)    $anime->my_score->text,
-                    'status'   => (int)    $anime->my_status->text,
+                    'episodes' => Convert::toInt   ($anime->my_watched_episodes->text),
+                    'score'    => Convert::toInt   ($anime->my_score->text),
+                    'status'   => Convert::toInt   ($anime->my_status->text),
                 ]
             ];
             array_push($results, $result);
@@ -41,18 +42,18 @@ class Anime
 
         $result = [
             'serie' => [
-                'id'        => (int)    $id,
-                'title'     => (string) Dom::get('h1')->text,
-                'title_eng' => (string) Dom::query('//*[@class="dark_text" and text()="English:"]/..')->content,
-                'title_jap' => (string) Dom::query('//*[@class="dark_text" and text()="Japanese:"]/..')->content,
-                'synonyms'  => (string) Dom::query('//*[@class="dark_text" and text()="Synonyms:"]/..')->content,
-                'ranked'    => (int)    str_replace('#', '', Dom::query('//*[@class="dark_text" and text()="Ranked:"]/..')->content),
-                'type'      => (string) Dom::query('//*[@class="dark_text" and text()="Type:"]/..')->content,
-                'episodes'  => (int)    Dom::query('//*[@class="dark_text" and text()="Episodes:"]/..')->content,
-                'status'    => (int)    $this->StatusNumber(Dom::query('//*[@class="dark_text" and text()="Status:"]/..')->content),
-                'genres'    => (array)  explode(', ', trim(str_replace('Genres:', '', Dom::query('//*[@class="dark_text" and text()="Genres:"]/..')->text))),
-                'score'     => (float)  Dom::query('//*[@class="dark_text" and text()="Score:"]/..')->content,
-                'synopsis'  => (string) preg_replace('/\s+/', ' ', Dom::query('//table//table//*[@valign="top"]')->content),
+                'id'        => Convert::toInt   ($id),
+                'title'     => Convert::toString(Dom::get('h1')->text),
+                'title_eng' => Convert::toString(Dom::query('//*[@class="dark_text" and text()="English:"]/..')->content),
+                'title_jap' => Convert::toString(Dom::query('//*[@class="dark_text" and text()="Japanese:"]/..')->content),
+                'synonyms'  => Convert::toString(Dom::query('//*[@class="dark_text" and text()="Synonyms:"]/..')->content),
+                'ranked'    => Convert::toInt   (Dom::query('//*[@class="dark_text" and text()="Ranked:"]/..')->content),
+                'type'      => Convert::toString(Dom::query('//*[@class="dark_text" and text()="Type:"]/..')->content),
+                'episodes'  => Convert::toInt   (Dom::query('//*[@class="dark_text" and text()="Episodes:"]/..')->content),
+                'status'    => Convert::toInt   ($this->StatusNumber(Dom::query('//*[@class="dark_text" and text()="Status:"]/..')->content)),
+                'genres'    => Convert::toArray (str_replace('Genres:', '', Dom::query('//*[@class="dark_text" and text()="Genres:"]/..')->text)),
+                'score'     => Convert::toFloat (Dom::query('//*[@class="dark_text" and text()="Score:"]/..')->content),
+                'synopsis'  => Convert::toString(Dom::query('//table//table//*[@valign="top"]')->content),
             ]
         ];
         return $result;
